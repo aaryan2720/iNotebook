4d32c20e69842765d95ebbd036ae78cb9dfd024b
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-10-31 09:53:53 +0530

    "Updated MongoDB connection options and error handling in db.js"

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-const mongoURI ='mongodb+srv://aryan:shalini2720@cluster0.0sltc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://aryan:shalini2720@cluster0.0sltc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI, { serverSelectionTimeoutMS: 30000 });
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+    });
     console.log('Connected to MongoDB successfully');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB:', error.message || error);
   }
 };
 
